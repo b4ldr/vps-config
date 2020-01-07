472a867d787c8602c7fda16a39705803b6608855
John Bond <jbond@wikimedia.org>
2020-01-07 20:42:30 +0100

    add dns firewall rule

--- manifests/loki.pp
+++ manifests/loki.pp
@@ -4,4 +4,15 @@ node 'loki.johnbond.org' {
     include firewall
     include my_fw::post
     include knot
+    firewall {'101 DNS ipv4':
+        proto   => 'all',
+        dport   => 53,
+        actions => 'accept',
+    }
+    firewall {'101 DNS ipv6':
+        proto    => 'all',
+        dport    => 53,
+        actions  => 'accept',
+        provider => 'ip6tables',
+    }
 }
