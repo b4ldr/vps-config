52ec8bc1cf6afadcae5bb8dcd5d4b424c58e2057
John Bond <jbond@wikimedia.org>
2020-01-09 18:20:23 +0100

    add cron job

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -17,6 +17,12 @@ class base (
         content => file('base/puppet-apply'),
         mode    => '0550',
     }
+    cron { 'puppet-apply':
+        ensure  => present,
+        minute  => 5,
+        command => '/usr/local/bin/puppet-apply',
+        require => File['/usr/local/bin/puppet-apply'],
+    }
     $users.each |String $user, Hash $config| {
         user{$user:
             * => $config['user'],
