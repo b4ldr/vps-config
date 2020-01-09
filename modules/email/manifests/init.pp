f31c4af6c96b2ac6bc051004383258f1c577f17e
John Bond <jbond@wikimedia.org>
2020-01-09 19:43:24 +0100

    add domain managment

--- modules/email/manifests/init.pp
+++ modules/email/manifests/init.pp
@@ -1,10 +1,11 @@
 #
 class email (
-    Stdlib::Unixpath $virtual_dir = '/etc/exim4/virtual_dir',
-    Stdlib::Unixpath $tls_cert = '/etc/ssl/certs/imaps.johnbond.org.pem',
-    Stdlib::Unixpath $tls_key = '/etc/ssl/private/imaps.johnbond.org.key',
-    Stdlib::Unixpath $dkim_key = '/etc/ssl/private/dkim.key',
-    Stdlib::Host $qualify_domain = 'johnbond.org'
+    Stdlib::Unixpath $virtual_dir    = '/etc/exim4/virtual_dir',
+    Stdlib::Unixpath $tls_cert       = '/etc/ssl/certs/imaps.johnbond.org.pem',
+    Stdlib::Unixpath $tls_key        = '/etc/ssl/private/imaps.johnbond.org.key',
+    Stdlib::Unixpath $dkim_key       = '/etc/ssl/private/dkim.key',
+    Stdlib::Host     $qualify_domain = 'johnbond.org',
+    Hash             $domains        = {},
 ) {
     ensure_packages(['exim4-daemon-heavy', 'procmail'])
     file { '/etc/exim4/exim4.conf':
@@ -18,4 +19,17 @@ class email (
         group  => 'Debian-exim',
         mode   => '0440',
     }
+    $domains.each |String $domain, Hash $config| {
+        $content = $config.has_key('aliases') ? {
+            true    => $config['aliases'].to_yaml,
+            default => '',
+        }
+        file {"${virtual_dir}/${domain}":
+            ensure  => file,
+            group   => 'Debian-exim',
+            mode    => '0440',
+            content => $content,
+        }
+    }
+
 }
