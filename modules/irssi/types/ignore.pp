7889f473e872ea7fbb56b83ce9dbe2934ea2c9ce
John Bond <jbond@wikimedia.org>
2020-02-07 21:05:30 +0100

    correct typos

--- modules/irssi/types/ignore.pp
+++ modules/irssi/types/ignore.pp
@@ -2,6 +2,6 @@ type Irssi::Ignore = Struct[{
     level     => Optional[Array[Enum['ALL', 'JOINS', 'QUITS', 'NO_ACT']]],
     mask      => Optional[String[1]],
     pattern   => Optional[String[1]],
-    excptions => Optional[Boolean],
+    exception => Optional[Boolean],
     regexp    => Optional[Boolean],
 }]
