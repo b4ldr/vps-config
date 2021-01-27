c5c974a710741ad4dbbe6da6b61379fc4d180491
John Bond <github@johnbond.org>
2021-01-27 11:48:07 +0100

    update certbot config

--- modules/certbot/manifests/init.pp
+++ modules/certbot/manifests/init.pp
@@ -1,19 +1,9 @@
 class certbot {
     ensure_packages(['certbot'])
     cron {'certbot_exim':
-        command => '/usr/bin/certbot certonly --post-hook "systemctl restart exim4 dovecot"',
+        command => '/usr/bin/certbot renew',
         minute  => 0,
         hour    => 11,
-    }
-    cron {'certbot_nel.johnbond.org':
-        command => '/usr/bin/certbot certonly --apache -d nel.johnbond.org',
-        minute  => 0,
-        hour    => 12,
-    }
-    cron {'certbot_attackplanner.co.uk':
-        command => '/usr/bin/certbot certonly --apache -d attackplanner.co.uk',
-        minute  => 0,
-        hour    => 13,
     }
 
 }
