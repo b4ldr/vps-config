6eceb52599d1737423a66f1d79cbf3c1d351f132
John Bond <github@johnbond.org>
2021-01-27 11:40:30 +0100

    two seperate crons

--- modules/certbot/manifests/init.pp
+++ modules/certbot/manifests/init.pp
@@ -1,14 +1,19 @@
 class certbot {
     ensure_packages(['certbot'])
     cron {'certbot_exim':
-        command => "/usr/bin/certbot certonly --post-hook 'systemctl restart exim4 dovecot'",
+        command => '/usr/bin/certbot certonly --post-hook "systemctl restart exim4 dovecot"',
         minute  => 0,
         hour    => 11,
     }
-    cron {'certbot_apache':
-        command => "/usr/bin/certbot certonly --apache -d nel.johnbond.org -d attackplanner.co.uk",
+    cron {'certbot_nel.johnbond.org':
+        command => '/usr/bin/certbot certonly --apache -d nel.johnbond.org',
         minute  => 0,
         hour    => 12,
+    }
+    cron {'certbot_attackplanner.co.uk':
+        command => '/usr/bin/certbot certonly --apache -d attackplanner.co.uk',
+        minute  => 0,
+        hour    => 13,
     }
 
 }
