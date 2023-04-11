f0970b76e078af57c123515c58b2c473bdda0e24
jbond <github@johnbond.org>
2023-04-11 11:49:32 +0200

    dont purg fw rules

--- manifests/01site.pp
+++ manifests/01site.pp
@@ -1,6 +1,7 @@
-resources { 'firewall':
-  purge => true,
-}
+# need to account for fail to ban
+#resources { 'firewall':
+#  purge => true,
+#}
 Firewall {
   before  => Class['my_fw::post'],
   require => Class['my_fw::pre'],
