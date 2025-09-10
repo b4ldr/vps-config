45a101ce4b60efebae853d4933a6dcc1e1542874
jbond <github@johnbond.org>
2025-09-10 18:58:23 +0200

    add cron-core

--- Puppetfile
+++ Puppetfile
@@ -2,11 +2,13 @@ moduledir '/etc/puppet/code/modules'
 mod 'puppetlabs/stdlib'
 mod 'puppetlabs/translate'
 mod 'puppetlabs/concat'
+mod 'puppetlabs/cron_core'
 mod 'puppetlabs/firewall'
 mod 'puppetlabs/apt'
 mod 'puppetlabs/apache'
 mod 'puppetlabs/vcsrepo'
 mod 'puppetlabs/inifile'
+mod 'puppetlabs/postgresql'
 mod 'camptocamp/systemd'
 mod 'knot',
   :git => 'https://github.com/b4ldr/puppet-knot',
--- manifests/loki.pp
+++ manifests/loki.pp
@@ -1,12 +1,13 @@
 node 'loki.johnbond.org' {
-    include puppet_apply
-    include my_fw::pre
-    include firewall
-    include my_fw::post
-    include certbot
-    include dns
-    include email
-    include http
-    include irssi
-    # include wireguard
+  include puppet_apply
+  include my_fw::pre
+  include firewall
+  include my_fw::post
+  include certbot
+  include dns
+  include email
+  include http
+  include irssi
+  include postgresql::server
+  # include wireguard
 }
